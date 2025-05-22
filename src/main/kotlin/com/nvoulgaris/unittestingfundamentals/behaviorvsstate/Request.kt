5d95f873700f6f44dedaa76f27f8a3f6e507b843
Nikos Voulgaris <nivoul@gmail.com>
2025-05-22 09:55:52 +0300

    Add the testing behavior vs testing state examples

---
+++ src/main/kotlin/com/nvoulgaris/unittestingfundamentals/behaviorvsstate/Request.kt
@@ -0,0 +1,12 @@
+package com.nvoulgaris.unittestingfundamentals.behaviorvsstate
+
+/**
+ * This is a dummy implementation, in order to support
+ * [com.nvoulgaris.unittestingfundamentals.readability.GatewaySpec]
+ */
+open class Request {
+
+  fun execute(): Boolean {
+    return true
+  }
+}
